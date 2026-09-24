8a11e800480c09fc2d6e15a8d22ceff27ee34d19
waquarahmad622-lgtm <waquarahmad622@gmail.com>
2026-09-25 02:16:47 +0530

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -2,6 +2,6 @@
 // Both values are safe to put in browser code when RLS is correctly configured.
 // NEVER put a secret key, service_role key, database password, or admin login here.
 window.KGN_CLOUD_CONFIG = Object.freeze({
-  url: '',
-  publishableKey: ''
+  url: 'https://vmvyubzpuncasirgjptn.supabase.co',
+  publishableKey: 'sb_publishable_GnOXcOlBLtxCjI2YOYrczA_dI-2KJL7'
 });
